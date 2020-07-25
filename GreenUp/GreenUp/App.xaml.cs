f81562ec437adfdb32c1d69e659773fd64b8913f
fadikamohamed <fadika.mohamed02@gmail.com>
2020-07-25 15:49:13 +0200

    project with base folders

--- GreenUp/GreenUp/App.xaml.cs
+++ GreenUp/GreenUp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GreenUp.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
---
+++ GreenUp/GreenUp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GreenUp.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        protected BaseViewModel()
+        {
+
+        }
+
+    }
+}
