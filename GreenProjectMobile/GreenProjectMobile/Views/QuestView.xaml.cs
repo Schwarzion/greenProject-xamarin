c2a260c327e9db84e9b68fff9b0f3ad27a430720
Coline <53483063+coline-tylec@users.noreply.github.com>
2020-09-25 22:33:27 +0200

    adding Quest View

---
+++ GreenProjectMobile/GreenProjectMobile/Views/QuestView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenProjectMobile.ViewsModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenProjectMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Quests : ContentPage
+    {
+        public Quests()
+        {
+            InitializeComponent();
+            var service = new QuestViewModel();
+            service.GetQuests();
+        }
+    }
+}
