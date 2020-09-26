b9bd0298d83e2c55abf4ddefd3898799d48b1bfe
Coline <53483063+coline-tylec@users.noreply.github.com>
2020-09-27 00:14:13 +0200

    view all quests successful

--- GreenProjectMobile/GreenProjectMobile/ViewsModels/QuestViewModel.cs
+++ GreenProjectMobile/GreenProjectMobile/ViewsModels/QuestViewModel.cs
@@ -13,10 +13,11 @@ using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using GreenProjectMobile.Views;
+using System.Runtime.CompilerServices;
 
 namespace GreenProjectMobile.ViewsModels
 {
-    class QuestViewModel : INotifyCollectionChanged
+    class QuestViewModel : INotifyPropertyChanged
     {
         readonly HttpClient client;
         public QuestViewModel()
@@ -27,67 +28,92 @@ namespace GreenProjectMobile.ViewsModels
         }
         public Quest Quests { get; private set; }
 
-        public ObservableCollection<Quest> questList { get; private set; }
+        private ObservableCollection<Quest> _questList;
 
-        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public ObservableCollection<Quest> questList
+        {
+            get
+            {
+                return _questList;
+            }
+            set
+            {
+                _questList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged ;
 
         public async void GetQuests()
         {
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await SecureStorage.GetAsync("Token"));
             string uri = Constants.Constants.BaseUrl + "allQuests";
-            HttpResponseMessage response = null;
             QuestsResult quests = new QuestsResult();
 
             try
             {
+                HttpResponseMessage response;
                 response = await client.GetAsync(uri);
                 if (response != null && response.IsSuccessStatusCode == true)
                 {
                     var contents = response.Content.ReadAsStringAsync().Result;
+                    Debug.WriteLine("DEBUG contents : " + contents);
                     quests = JsonConvert.DeserializeObject<QuestsResult>(contents);
+                    Debug.WriteLine("DEBUG quests : " + quests);
                     List<Quest> list = new List<Quest>(quests.quests);
+                    Debug.WriteLine("DEBUG list : " + list);
                     questList = new ObservableCollection<Quest>(list as List<Quest>);
                     foreach (Quest elem in questList)
                     {
-                        Debug.WriteLine(elem);
+                        Debug.WriteLine("DEBUG elem " + elem);
                     }
                 }
             }
             catch (Exception ex)
             {
-                Debug.WriteLine($"Exception Quests: {ex}");
+                Debug.WriteLine($"Exception Quests in GetQuests: {ex}");
             }
         }
 
-        public async Task<ObservableCollection<Quest>> GetQuestsRequest()
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await SecureStorage.GetAsync("Token"));
-            string uri = Constants.Constants.BaseUrl + "allQuests";
-            HttpResponseMessage response = null;
-            QuestsResult quests = new QuestsResult();
-            List<Quest> list = new List<Quest>();
-            try
+            var handler = PropertyChanged;
+            if (handler != null)
             {
-                response = await client.GetAsync(uri);
-                if (response != null && response.IsSuccessStatusCode == true)
-                {
-                    var contents = response.Content.ReadAsStringAsync().Result;
-                    quests = JsonConvert.DeserializeObject<QuestsResult>(contents);
-                    list = quests.quests;
-                }
+                handler(this, new PropertyChangedEventArgs(propertyName));
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Exception Quests: {ex}");
-            }
-            ObservableCollection<Quest> obsList = new ObservableCollection<Quest>(list as List<Quest>);
-            foreach (Quest elem in obsList)
-            {
-                Debug.WriteLine(elem.name);
-            }
-            return obsList;
         }
+
+        //public async Task<ObservableCollection<Quest>> GetQuestsRequest()
+        //{
+
+        //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await SecureStorage.GetAsync("Token"));
+        //    string uri = Constants.Constants.BaseUrl + "allQuests";
+        //    HttpResponseMessage response = null;
+        //    QuestsResult quests = new QuestsResult();
+        //    List<Quest> list = new List<Quest>();
+        //    try
+        //    {
+        //        response = await client.GetAsync(uri);
+        //        if (response != null && response.IsSuccessStatusCode == true)
+        //        {
+        //            var contents = response.Content.ReadAsStringAsync().Result;
+        //            quests = JsonConvert.DeserializeObject<QuestsResult>(contents);
+        //            list = quests.quests;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine($"Exception Quests in GetQuestsRequest : {ex}");
+        //    }
+        //    ObservableCollection<Quest> obsList = new ObservableCollection<Quest>(list as List<Quest>);
+        //    foreach (Quest elem in obsList)
+        //    {
+        //        Debug.WriteLine("DEBUG elem.name : " + elem.name);
+        //    }
+        //    return obsList;
+        //}
 
     }
 }
