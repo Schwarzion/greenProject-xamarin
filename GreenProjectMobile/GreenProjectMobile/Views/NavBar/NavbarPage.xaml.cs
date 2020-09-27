f5710f3f9d108a22a9e5c6c450c1ab2288a922f4
@fadika.mohamed02 <mohamed.fadika@canal-tech.fr>
2020-09-27 14:55:55 +0200

    profile page wip

--- GreenProjectMobile/GreenProjectMobile/Views/NavBar/NavbarPage.xaml.cs
+++ GreenProjectMobile/GreenProjectMobile/Views/NavBar/NavbarPage.xaml.cs
@@ -22,6 +22,8 @@ namespace GreenProjectMobile.Views
             btnMain.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.MainPage);
             btnAbout.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.AboutPage);
             btnTips.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.Tips);
+            btnProfile.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.ProfileView);
+
             btnLogout.Clicked += (s, e) =>
             {
                 SecureStorage.Remove("Token");
