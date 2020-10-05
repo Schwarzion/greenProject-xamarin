1cf9bc3250b0085afd0acf0c689b86cb9910b417
Thomas Vionnet <th.vionnet@gmail.com>
2020-10-05 21:43:38 +0200

    feat(registerPage): add style to register page | add content to About page | harmonize some style here and there

--- GreenProjectMobile/GreenProjectMobile/Views/AboutPage.xaml.cs
+++ GreenProjectMobile/GreenProjectMobile/Views/AboutPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,10 @@ namespace GreenProjectMobile.Views
         public AboutPage()
         {
             InitializeComponent();
+            btnContact.Clicked += (sender, e) =>
+            {
+                Launcher.OpenAsync(new Uri("mailto:th.vionnet@gmail.com"));
+            };
         }
     }
 }
