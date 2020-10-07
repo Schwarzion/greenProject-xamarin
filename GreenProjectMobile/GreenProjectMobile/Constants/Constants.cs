85efd06308fe3d77915d1d2cd6b4c702bc5957e6
schwarzion <victor.lehouck@gmail.com>
2020-10-07 21:13:43 +0200

    [FIX] Datetime validation token

--- GreenProjectMobile/GreenProjectMobile/Constants/Constants.cs
+++ GreenProjectMobile/GreenProjectMobile/Constants/Constants.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,3 +10,17 @@ namespace GreenProjectMobile.Constants
         public const string BaseUrl = "http://192.168.0.10:8000/api/";
     }
 }
+=======
+﻿using Android.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenProjectMobile.Constants
+{
+    public static class Constants
+    {
+        public const string BaseUrl = "http://192.168.0.16:8000/api/";
+    }
+}
+>>>>>>> d6648ea... [FIX] Datetime validation token
