2fd7bc8d899804c81ee037758a17fd33b04173c9
schwarzion <victor.lehouck@gmail.com>
2020-10-09 18:04:05 +0200

    Design tips

--- GreenProjectMobile/GreenProjectMobile/Constants/Constants.cs
+++ GreenProjectMobile/GreenProjectMobile/Constants/Constants.cs
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GreenProjectMobile.Constants
-{
-    public static class Constants
-    {
-        public const string BaseUrl = "http://192.168.43.203:8000/api/";
-    }
-}
-=======
-﻿using Android.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,7 +6,11 @@ namespace GreenProjectMobile.Constants
 {
     public static class Constants
     {
+<<<<<<< HEAD
+        public const string BaseUrl = "http://192.168.43.203:8000/api/";
+=======
         public const string BaseUrl = "http://192.168.0.16:8000/api/";
+>>>>>>> 88c24c4... Design tips
     }
 }
->>>>>>> d6648ea... [FIX] Datetime validation token
+
