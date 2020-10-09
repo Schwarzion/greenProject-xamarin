db9ed4692b3082b45d9e7d427ab28c9e654248fa
Thomas Vionnet <th.vionnet@gmail.com>
2020-10-09 23:27:20 +0200

    feat(Quests): add method to AcceptQuest

--- GreenProjectMobile/GreenProjectMobile/ViewsModels/QuestDetailViewModel.cs
+++ GreenProjectMobile/GreenProjectMobile/ViewsModels/QuestDetailViewModel.cs
@@ -1,24 +1,53 @@
 ﻿using GreenProjectMobile.Models;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace GreenProjectMobile.ViewsModels
 {
     public class QuestDetailViewModel
     {
+
+        readonly HttpClient client;
+
         private Quest _quest;
         public Quest Quest
         {
-        get => _quest;
+            get => _quest;
             set
             {
                 _quest = value;
             }
         }
+
+        public ICommand AddQuestCommand { protected set; get; }
+
         public QuestDetailViewModel (Quest quest)
         {
             this.Quest = quest;
+            AddQuestCommand = new Command(OnAcceptQuest);
+        }
+
+
+        public void OnAcceptQuest()
+        {
+            AcceptQuest("addQuest/{id}", _quest.id);
+        }
+
+        public async void  AcceptQuest(string path, int questId)
+        {
+            string uri = Constants.Constants.BaseUrl + path;
+            HttpResponseMessage response = null;
+
+            try
+            {
+                response = await client.PostAsync(uri, content);
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erreur connexion", "La connexion au serveur a échouée.", "Ok");
+            }
         }
     }
 }
